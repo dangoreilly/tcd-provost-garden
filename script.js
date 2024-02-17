6dc67fffc67b1a60be74936082cf2ddeb5734bac
Dan O'Reilly <dangoreilly@gmail.com>
2024-02-17 22:41:05 +0000

    .

--- script.js
+++ script.js
@@ -26,21 +26,104 @@ function initMap(MAP_SIZE, BUFFER, BACKGROUND_IMAGE, data) {
     // Create the image overlay
     let background = L.imageOverlay(BACKGROUND_IMAGE, map_bounds).addTo(map);
 
+    // When the map is clicked, print the coordinates to the console
+    // And reset all the icons 
     map.on('click', function(e){
-        console.log(e.latlng.lat, e.latlng.lng);
+        console.log([e.latlng['lat'], e.latlng['lng']]);
+        resetAllIcons(markers);
+        updateCard();
+    });
+    
+    // Create an array for the markers
+    let markers = [];
+
+    // Loop through the data and add the markers
+    for (let i = 0; i < data.length; i++){
+        // Create a divIcon for the marker
+        let icon = L.divIcon({
+            className: 'custom-div-icon',
+            html: "<div class='marker-pin'><div class='marker-text'>" + data[i].id + "</div></div>"
+        });
+
+        // Create the marker
+        let marker = L.marker(data[i].coordinates, {icon: icon}).addTo(map);
+        markers.push({marker : marker, data: data[i]});
+
+        // Add the click event
+        marker.on('click', function(e){
+            // Update the card with the new information
+            updateCard(
+                data[i].image,
+                data[i].title,
+                data[i].species,
+                data[i].description,
+                data[i].carbon,
+                data[i].age
+            );
+
+            // Make a pulsing version of the marker's icon
+            let pulsingIcon = L.divIcon({
+                className: 'custom-div-icon',
+                html: "<div class='marker-pin marker-flash'><div class='marker-text'>" + data[i].id + "</div></div>"
+            });
+
+            // Set the marker to the pulsing icon
+            marker.setIcon(pulsingIcon);
+
+        });
+    }
+    
+    // When the map is clicked, print the coordinates to the console
+    // And reset all the icons 
+    map.on('click', function(e){
+        console.log([e.latlng['lat'], e.latlng['lng']]);
+        resetAllIcons(markers);
     });
 
-    //TEST: Update the card
-    // updateCard(
-    //     "images/oregon-maple.png",
-    //     "oregon maple",
-    //     "treeus latinus",
-    //     "it's a tree",
-    //     17,
-    //     240
-    
-    // );
+    // Check the url for a query string
+    let url = new URL(window.location.href);
+    let query = url.searchParams.get("id");
+    if (query != null){
+        // Find the marker with the matching id
+        for (let i = 0; i < markers.length; i++){
+            if (markers[i].data.id == query){
+                // Update the card with the new information
+                updateCard(
+                    markers[i].data.image,
+                    markers[i].data.title,
+                    markers[i].data.species,
+                    markers[i].data.description,
+                    markers[i].data.carbon,
+                    markers[i].data.age
+                );
 
+                // Pan to the marker
+                map.panTo(markers[i].data.coordinates);
+
+                // Make a pulsing version of the marker's icon
+                let pulsingIcon = L.divIcon({
+                    className: 'custom-div-icon',
+                    html: "<div class='marker-pin marker-flash'><div class='marker-text'>" + markers[i].data.id + "</div></div>"
+                });
+
+                // Set the marker to the pulsing icon
+                markers[i].marker.setIcon(pulsingIcon);
+            }
+        }
+    }
+
+}
+
+function resetAllIcons(markers, data){
+    // Loops through all the markers and resets their icons
+    for (let i = 0; i < markers.length; i++){
+
+        let icon = L.divIcon({
+            className: 'custom-div-icon',
+            html: "<div class='marker-pin'><div class='marker-text'>" + markers[i].data.id + "</div></div>"
+        });
+        markers[i].marker.setIcon(icon);
+    }
 }
 
 function updateCard(image=null, title, species, description, carbon, age){
